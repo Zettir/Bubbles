a7c1c8c57d9a76e51feec5dab5347a73c377f2fe
Zettir <gerdes@outlook.dk>
2018-05-14 11:52:50 +0200

    All is Awesome

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 "use strict";
 
-const divs = document.querySelectorAll("#aa div");
+const divs = document.querySelectorAll("#app div");
 
 divs.forEach( function(div) {
-    div.style.border = "2px solid #000";
-})
+    // div.style.transform = `translate(50vw, 50vh)`;
+    div.addEventListener("mouseenter", function() {
+        div.style.transform = `translate(${Math.random()*100}vw, ${Math.random()*100}vh)`;
+        div.style.backgroundColor = `hsl(${Math.random()*360},${Math.random()*100}%,${Math.random()*100}%)`;
+    });
+});
